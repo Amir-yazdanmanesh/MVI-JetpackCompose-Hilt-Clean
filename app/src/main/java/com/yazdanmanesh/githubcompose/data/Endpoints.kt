e7378b9a3c3e9aa919e81d90f5ee4a8303653ec9
amir <amir.yazdanmanesh@gmail.com>
2024-10-18 12:31:40 +0330

    Implement EntryPoint

--- app/src/main/java/com/yazdanmanesh/githubcompose/data/Endpoints.kt
+++ app/src/main/java/com/yazdanmanesh/githubcompose/data/Endpoints.kt
@@ -3,7 +3,7 @@ package com.yazdanmanesh.githubcompose.data
 object Endpoints {
     const val BASE_URL = "https://api.github.com/"
 
-    const val GET_USERS = "users?since=51234842"
+    const val GET_USERS = "users?since=16717834"
     const val GET_USER = "users/{userLogin}"
     const val GET_REPOS_BY_USER = "users/{userLogin}/repos?sort=stars&order=desc"
 }
