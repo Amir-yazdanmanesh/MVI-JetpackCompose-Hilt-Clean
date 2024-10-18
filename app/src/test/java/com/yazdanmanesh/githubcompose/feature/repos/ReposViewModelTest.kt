a38b9230308f50c6e408f9c2cd2d62d88a68e333
amir <amir.yazdanmanesh@gmail.com>
2024-10-18 12:32:19 +0330

    Implement Tests

---
+++ app/src/test/java/com/yazdanmanesh/githubcompose/feature/repos/ReposViewModelTest.kt
@@ -0,0 +1,79 @@
+package com.yazdanmanesh.githubcompose.feature.repos
+
+import com.yazdanmanesh.githubcompose.data.GithubRepository
+import com.yazdanmanesh.githubcompose.data.model.Repo
+import com.yazdanmanesh.githubcompose.data.model.UserDetail
+import com.yazdanmanesh.githubcompose.ui.feature.repos.ReposContract
+import com.yazdanmanesh.githubcompose.ui.feature.repos.ReposViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ReposViewModelTest{
+    private val githubRepository = mockk<GithubRepository>()
+
+    @Before
+    fun setUp(){
+        Dispatchers.setMain(Dispatchers.Unconfined)
+    }
+    @Test
+    fun `when view model initialized then should emit initial view state first`() = runTest {
+        val expectedInitialViewState = ReposContract.State(
+            user = null,
+            reposList = emptyList(),
+            isUserLoading = true,
+            isReposLoading = true,
+            isError = false
+        )
+        val viewmodel = ReposViewModel(githubRepository)
+        viewmodel.init("testUserId")
+        assertEquals(expectedInitialViewState,viewmodel.viewState.value)
+    }
+
+    @Test
+    fun `when fetchInitialData called then should emit user and repos state`() = runTest {
+        val userId = "testUserId"
+        val user = UserDetail(avatarUrl = "avatar_url", htmlUrl = "html_url")
+        val repos = listOf(Repo(id = 1, name = "Repo 1", description = "Description 1"))
+        val expectedViewState = ReposContract.State(
+            user = user,
+            reposList = repos,
+            isUserLoading = false,
+            isReposLoading = false,
+            isError = false
+        )
+        coEvery { githubRepository.getUser(userId) } returns Result.success(user)
+        coEvery { githubRepository.getRepos(userId) } returns Result.success(repos)
+
+        val viewmodel = ReposViewModel(githubRepository)
+        viewmodel.init(userId)
+        assertEquals(expectedViewState,viewmodel.viewState.value)
+    }
+
+    @Test
+    fun `test state after repos retrieval failure`() = runTest {
+        val userId = "testUserId"
+        val user = UserDetail(avatarUrl = "avatar_url", htmlUrl = "html_url")
+        val expectedViewState = ReposContract.State(
+            user = user,
+            reposList = emptyList(),
+            isUserLoading = false,
+            isReposLoading = false,
+            isError = true
+        )
+
+        coEvery { githubRepository.getUser(userId) } returns Result.success(user)
+        coEvery { githubRepository.getRepos(userId) } returns Result.failure(Exception("Repos retrieval failed"))
+
+        val viewmodel = ReposViewModel(githubRepository)
+        viewmodel.init(userId)
+        assertEquals(expectedViewState,viewmodel.viewState.value)
+    }
+}
---
+++ app/src/test/java/com/yazdanmanesh/githubcompose/feature/users/UsersViewModelTest.kt
@@ -0,0 +1,93 @@
+package com.yazdanmanesh.githubcompose.feature.users
+
+import androidx.compose.runtime.snapshots.Snapshot
+import com.yazdanmanesh.githubcompose.MainCoroutineRule
+import com.yazdanmanesh.githubcompose.data.GithubRepository
+import com.yazdanmanesh.githubcompose.data.model.User
+import com.yazdanmanesh.githubcompose.ui.feature.users.UsersContract
+import com.yazdanmanesh.githubcompose.ui.feature.users.UsersViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@DelicateCoroutinesApi
+@OptIn(ExperimentalCoroutinesApi::class)
+class UsersViewModelTest {
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private val githubRepository = mockk<GithubRepository>()
+
+    @Test
+    fun `When view model initialized then should emit initial view state first`() = runTest {
+        // Given
+        val expectedInitialViewState = UsersContract.State(
+            users = emptyList(),
+            isLoading = true,
+            isError = false
+        )
+
+        // When
+        val viewModel = UsersViewModel(githubRepository)
+
+        // Then
+        assertEquals(expectedInitialViewState, viewModel.viewState.value)
+    }
+
+    @Test
+    fun `When getUsers called then should emit a view state`() = runTest {
+        // Given
+        val users = listOf(User(userId = "16717834"))
+        val expectedViewState = UsersContract.State(
+            users = users,
+            isLoading = false,
+            isError = false
+        )
+        coEvery { githubRepository.getUsers() } returns Result.success(users)
+
+        // When
+        val viewModel = UsersViewModel(githubRepository)
+
+        // Then
+        assertEquals(expectedViewState, viewModel.viewState.value)
+    }
+
+    @Test
+    fun `When `() = runTest {
+
+        // Given
+        val users = listOf(User(userId = "16717834"))
+        val expectedViewState = UsersContract.State(
+            users = users,
+            isLoading = false,
+            isError = false
+        )
+        coEvery { githubRepository.getUsers() } returns Result.success(users)
+
+        // When
+        val snapshot = Snapshot.takeMutableSnapshot(
+//            readObserver = {
+//                println(it)
+//            },
+            writeObserver = {
+                println(it)
+            }
+        )
+
+        snapshot.enter {
+            val viewModel = UsersViewModel(githubRepository)
+
+            // Then
+            assertEquals(expectedViewState, viewModel.viewState.value)
+        }
+
+        snapshot.apply()
+    }
+
+}
