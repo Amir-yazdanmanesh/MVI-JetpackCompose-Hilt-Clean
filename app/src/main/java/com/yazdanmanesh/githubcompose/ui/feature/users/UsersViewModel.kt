0bce792670594c0ce203c2e16c254f5ee239dc49
amir <amir.yazdanmanesh@gmail.com>
2024-10-18 12:01:57 +0330

    Implement view model

---
+++ app/src/main/java/com/yazdanmanesh/githubcompose/ui/feature/users/UsersViewModel.kt
@@ -0,0 +1,44 @@
+package com.yazdanmanesh.githubcompose.ui.feature.users
+
+import androidx.lifecycle.viewModelScope
+import com.yazdanmanesh.githubcompose.data.GithubRepository
+import com.yazdanmanesh.githubcompose.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UsersViewModel @Inject constructor(
+    private val githubRepository: GithubRepository
+) : BaseViewModel<UsersContract.Event, UsersContract.State, UsersContract.Effect>() {
+
+    init { getUsers() }
+
+    override fun setInitialState() = UsersContract.State(
+        users = emptyList(),
+        isLoading = true,
+        isError = false,
+    )
+
+    override fun handleEvents(event: UsersContract.Event) {
+        when (event) {
+            is UsersContract.Event.UserSelection -> setEffect { UsersContract.Effect.Navigation.ToRepos(event.user.userId) }
+            is UsersContract.Event.Retry -> getUsers()
+        }
+    }
+
+    fun getUsers() {
+        viewModelScope.launch {
+            setState { copy(isLoading = true, isError = false) }
+
+            githubRepository.getUsers()
+                .onSuccess { users ->
+                    setState { copy(users = users, isLoading = false) }
+                    setEffect { UsersContract.Effect.DataWasLoaded }
+                }
+                .onFailure {
+                    setState { copy(isError = true, isLoading = false) }
+                }
+        }
+    }
+}
